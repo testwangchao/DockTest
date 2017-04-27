73c3bdd370f33f60932eaad189f10285e418fb83
wangchao <15201403874@163.com>
2017-04-27 15:05:30 +0800

    清空

--- dockTest.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-import os
-way = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.mkdir(way + 'mydockerbuild')
-os.chdir(way+'mydockerbuild')
-with open('Dockerfile','w+') as file:
-    file.write("FROM docker/whalesay:latest\nRUN apt-get -y update && apt-get install -y fortunes\nCMD /usr/games/fortune -a | cowsay")
-    print os.getcwd()
